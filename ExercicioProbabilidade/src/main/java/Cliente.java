4c62884b887835fcc5c6dda6eedd7aaf1c35b135
João Vitor Fonseca <jv.fonseca999@gmail.com>
2022-03-21 12:00:24 -0300

    parcial code

--- ExercicioProbabilidade/src/main/java/Cliente.java
+++ ExercicioProbabilidade/src/main/java/Cliente.java
@@ -1,10 +1,12 @@
 import java.util.Random;
 
 public class Cliente {
-    public int tipo;            // 0 = tratar negócios (2:30s) (30%)
-                                // 1 = individual (1:00s) (50%)
-                                // 2 = dúvidas (2:30s) (20%)
-    public float tempoServico;
+    private int tipo;            // 0 = tratar negócios (2:30s) (30%)   (150s)
+                                // 1 = individual (1:00s) (50%)         (60s)
+                                // 2 = dúvidas (0:30s) (20%)            (30s)
+
+
+    private double tempoServico; //Em segundos
 
     Random gerador = new Random();
 
@@ -12,12 +14,15 @@ public class Cliente {
         float r = gerador.nextInt(9)+1; // inteiros no intervalo aberto entre 1 e 10;
         if(r<=3){
             this.tipo = 0;
+            tempoServico = 150;
         }
-        else if (r>=8){
-            this.tipo = 2
+        else if (r<=8){
+            this.tipo = 1;
+            tempoServico = 60;
         }
         else {
-            this.tipo = 1;
+            this.tipo = 2;
+            tempoServico = 30;
         }
     }
 }
--- ExercicioProbabilidade/src/main/java/Main.java
+++ ExercicioProbabilidade/src/main/java/Main.java
@@ -1,26 +1,29 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        int tipo0 = 0;
-        int tipo1 = 0;
-        int tipo2 = 0;
+        double tempoSimulacao;
+        double tempoDecorrido = 0;
 
-        for(int i = 0;i<100;i++){
-            Cliente c = new Cliente();
-            System.out.println(c.tipo);
-            if(c.tipo==0){
-                tipo0++;
-            }
-            if(c.tipo==1){
-                tipo1++;
-            }
-            if(c.tipo==2){
-                tipo2++;
-            }
-            System.out.println("-------------------------------");
-            System.out.println("Tipo 0 (30%):  "+tipo0);
-            System.out.println("Tipo 1 (50%):  "+tipo1);
-            System.out.println("Tipo 2 (20%):  "+tipo2);
+        double taxaClientes;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Primeirameente, informe o tempo a ser simulado em segundos");
+        tempoSimulacao = scan.nextDouble();
+
+        System.out.println("Informe o tempo médio de chegada de clientes em segundo");
+        taxaClientes =  scan.nextDouble();
+
+        while(tempoSimulacao>=tempoDecorrido){
+            double esperaCliente = taxaClientes;// Aleatorizar isso
+
+
+
+            tempoDecorrido+=Time.
         }
+
+
     }
 }
