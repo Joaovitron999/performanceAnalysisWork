05e59d847d0a7b376ec996f4fc9d238cb82a7730
João Vitor Fonseca <61283617+Joaovitron999@users.noreply.github.com>
2022-03-21 09:15:17 -0300

    inicial files

---
+++ ExercicioProbabilidade/src/main/java/Cliente.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Cliente {
+    public int tipo;            // 0 = tratar negócios (2:30s) (30%)
+                                // 1 = individual (1:00s) (50%)
+                                // 2 = dúvidas (2:30s) (20%)
+    public float tempoServico;
+
+    Random gerador = new Random();
+
+    public Cliente() {
+        float r = 
+    }
+}
